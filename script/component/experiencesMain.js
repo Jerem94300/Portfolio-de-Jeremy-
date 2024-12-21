14953333c12a2845a302ce4eba3108335a1b6014
Jeremy ABELARD <abelardjeremy1@gmail.com>
2024-12-21 19:34:15 +0100

    maj pf

--- script/component/experiencesMain.js
+++ script/component/experiencesMain.js
@@ -5,10 +5,10 @@ export const Experiences = () => {
 
        <div class="experience__container fade-in">
         <div class="experience__card" id="military">
-          <h2 class="title">Expériences Militaires</h2>
+          <h2 class="title">Military Experience</h2>
         </div>
         <div class="experience__card" id="webdev">
-          <h2 class="title">Développement Web</h2>
+          <h2 class="title">Web Developer</h2>
         </div>
       </div>
    
