fb1ccb3feea4bd21bb300f4833140d30a4b87ac1
Jeremy ABELARD <abelardjeremy1@gmail.com>
2024-12-21 20:27:53 +0100

    maj pf

--- script/component/home.js
+++ script/component/home.js
@@ -6,7 +6,7 @@ export const Home = () => {
           <h2 class="title__main">Full-Stack Web & Mobile Developer</h2>
           <h1 class="title__h2">Hi, I am <span class="name__main">Jérémy</span></h1>
           <p class="paragraph__main">Passionate web developer, I create high-performing, custom websites for an optimal user experience.</p>
-          <button class="btn__download" onclick="window.location.href='assets/documents/CV_Jérémy _Abélard.pdf'">Download CV</button>
+          <button class="btn__download" onclick="window.location.href='assets/documents/CV.pdf'">Download CV</button>
         </div>
         <div class="photo__accueil">
           <img src="images/Photoroom_20241016_153653.jpeg" alt="Photo de Jérémy" class="photo__item">
