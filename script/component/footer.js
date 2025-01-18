f9b23ebb0d812ad1ee6366f10729810c997af2b5
Jeremy ABELARD <abelardjeremy1@gmail.com>
2025-01-18 09:33:50 +0100

    maj pf

--- script/component/footer.js
+++ script/component/footer.js
@@ -3,8 +3,6 @@ export const Footer = () => {
     return`
     <footer class="footer">
     <ul class="footer__nav__list">
-      <li><a href="https://www.linkedin.com/in/jérémy-abélard-512a762b1?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2FfWVAm4zRKiDAfDka2D46w%3D%3D" class="nav__link nav__link__footer"><i class="bi bi-facebook"></i></a></li>
-      <li><a href="https://www.linkedin.com/in/jérémy-abélard-512a762b1?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2FfWVAm4zRKiDAfDka2D46w%3D%3D" class="nav__link nav__link__footer"><i class="bi bi-instagram"></i></a></li>
       <li><a href="https://www.linkedin.com/in/jérémy-abélard-512a762b1?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2FfWVAm4zRKiDAfDka2D46w%3D%3D" class="nav__link nav__link__footer"><i class="bi bi-linkedin"></i></a></li>
     </ul>
         <p class="copyright">&copy; 2024 - Jérémy Abelard</p>
