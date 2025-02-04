051a6417f87dda7f314bbeac94c8c81f72b64dc4
Jeremy ABELARD <abelardjeremy1@gmail.com>
2025-02-04 20:03:10 +0100

    maj pf

--- script/component/expDevMain.js
+++ script/component/expDevMain.js
@@ -89,9 +89,9 @@ export const ExpDev = (data) => {
 
           
         </div>
-          <div class="experience__card" id="military"   onclick="window.location.href='#'">
-  
-          <h2 class="title">Military Experience</h2>
+          <div class="experience__card" id="military"   onclick="window.location.href='https://petitsplats.jeremyabelard.fr'">
+        <img src="assets/images/Recette13.jpg" alt="Logo recette13" class="petits__plats__logo">
+
         </div>
         <div class="experience__card" id="webdev"  onclick="window.location.href='expDev.html'">
           <h2 class="title">Web Developer</h2>
